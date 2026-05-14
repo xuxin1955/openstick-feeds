d09f77ece8e5ae4000e724503100502c1c98f2db
xuxin1955 <x2874836915@qq.com>
2026-05-14 15:35:40 +0800

    update

---
+++ utils/luci-app-temp-status/htdocs/luci-static/resources/view/status/include/29_temp.js
@@ -0,0 +1,89 @@
+'use strict';
+'require baseclass';
+'require rpc';
+
+return baseclass.extend({
+	title       : _('温度'),
+
+	tempWarning : 90,
+
+	tempCritical: 100,
+
+	callTempStatus: rpc.declare({
+		object: 'luci.temp-status',
+		method: 'getTempStatus',
+		expect: { '': {} }
+	}),
+
+	load: function() {
+		return L.resolveDefault(this.callTempStatus(), null);
+	},
+
+	render: function(tempData) {
+		if(!tempData) return;
+
+		let tempTable = E('table', { 'class': 'table' },
+			E('tr', { 'class': 'tr table-titles' }, [
+				E('th', { 'class': 'th left', 'width': '33%' }, _('传感器')),
+				E('th', { 'class': 'th left' }, _('温度')),
+			])
+		);
+
+		for(let [k, v] of Object.entries(tempData)) {
+			v.sort((a, b) => (a.number > b.number) ? 1 : (a.number < b.number) ? -1 : 0)
+
+			for(let i of Object.values(v)) {
+				let sensor = i.title || i.item;
+				if(i.item.indexOf('hwmon')>=0) {
+					continue;
+				};
+				if(i.sources === undefined) {
+					continue;
+				};
+
+				for(let j of i.sources) {
+					let temp = j.temp;
+					let name = (j.label !== undefined) ? sensor + " / " + j.label :
+						(j.item !== undefined) ? sensor + " / " + j.item.replace(/_input$/, "") : sensor
+
+					if(temp !== undefined) {
+						temp = Number((temp / 1000).toFixed(1));
+					};
+
+					let cellStyle = (temp >= this.tempCritical) ?
+						'color:#f5163b !important; font-weight:bold !important' :
+						(temp >= this.tempWarning) ?
+							'color:#ff821c !important; font-weight:bold !important' : null;
+
+					tempTable.append(
+						E('tr', { 'class': 'tr' }, [
+							E('td', {
+									'class'     : 'td left',
+									'style'     : cellStyle,
+									'data-title': _('传感器')
+								},
+								name
+							),
+							E('td', { 'class'  : 'td left',
+									'style'     : cellStyle,
+									'data-title': _('温度')
+								},
+								(temp === undefined) ? '-' : temp + ' °C'),
+						])
+					);
+				};
+			};
+		};
+
+		if(tempTable.childNodes.length === 1) {
+			tempTable.append(
+				E('tr', { 'class': 'tr placeholder' },
+					E('td', { 'class': 'td' },
+						E('em', {}, _('No temperature sensors available'))
+					)
+				)
+			);
+		};
+		return tempTable;
+	},
+});
